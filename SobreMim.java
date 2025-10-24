eaa08be509c5c9d66025ca5855d337460a505ab6
gabriela-holanda <gabrielagithub@gmail.com>
2025-10-23 21:52:32 -0300

    Add files via upload
    Mostrar nome e curso e "Eu quero estudar Java!".

---
+++ SobreMim.java
@@ -0,0 +1,7 @@
+public class SobreMim {
+    public static void main(String[] args) {
+        System.out.println("Meu nome é Gabriela");
+        System.out.println("Eu curso devs");
+        System.out.println("Eu quero aprender Java!");
+    }
+}
