05f33bd6bda3f6177d90755d1d3d0d3b22fe589c
gabriela-holanda <gabrielagithub@gmail.com>
2025-10-24 17:57:27 -0300

    Add files via upload
    Converter idade em anos, para meses e dias.

---
+++ ConversãoIdade.java
@@ -0,0 +1,10 @@
+public class ConversãoIdade {
+    public static void main(String[] args) {
+        
+        //Declarar variavel
+        int idade = 15;
+
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Igual a " + idade*12 + " meses ou " + idade*365 + " dias.");
+    }
+}
