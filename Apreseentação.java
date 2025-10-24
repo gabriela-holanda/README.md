516e63a7e5075e03cb725d553021b018608977cb
gabriela-holanda <gabrielagithub@gmail.com>
2025-10-23 21:49:01 -0300

    Add files via upload
    Mostrar nome completo, idade e cidade.

---
+++ Apreseentação.java
@@ -0,0 +1,14 @@
+public class Apreseentação {
+    public static void main(String[] args) {
+        //Declarar variáveis e tipos de dados
+
+        String nome = "Gabriela Andrade Holanda";
+        int idade = 15;
+        String cidade = "Acopiara";
+
+        //exibir informações
+        System.out.println("Nome completo: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Cidade: " + cidade);
+    }
+}
