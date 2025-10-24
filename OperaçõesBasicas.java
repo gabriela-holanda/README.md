714ca3c5e9721a6e8f41b90be42242a6311aa326
gabriela-holanda <gabrielagithub@gmail.com>
2025-10-23 21:54:55 -0300

    Add files via upload
    Calcular as quatro operações básicas.

---
+++ OperaçõesBasicas.java
@@ -0,0 +1,18 @@
+public class OperaçõesBasicas {
+    public static void main(String[] args) {
+        
+        //Declarar variaveis
+        int n = 4;
+        int m = 2;
+
+        double soma = n + m;
+        double subtração = n - m;
+        double multiplicação = n * m;
+        double divisão = n/m;
+
+        System.out.println("A soma é: " + soma);
+        System.out.println("A subtração é: " + subtração);
+        System.out.println("A multiplicação é: " + multiplicação);
+        System.out.println("A divisão é: " + divisão);
+    }
+}
