06c75220a1233bb97b349ef1eaafc0516a2859e3
gabriela-holanda <gabrielagithub@gmail.com>
2025-10-24 18:22:05 -0300

    Add files via upload
    Um código que exiba uma frase concatenada com nome e idade juntos na mesma linha.

---
+++ Q1Parte2.java
@@ -0,0 +1,12 @@
+public class Q1Parte2 {
+    public static void main(String[] args) {
+        
+        //Declarar variaveis
+        String nome = "Gabriela";
+        int idade = 15;
+
+        //Exibir
+        System.out.println("Meu nome é " + nome + " e tenho " + idade + " anos.");
+    }
+    
+}
