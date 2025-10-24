ca1b37eeead012657f313e7e18c3b74dfde76404
gabriela-holanda <gabrielagithub@gmail.com>
2025-10-24 18:22:57 -0300

    Add files via upload
    Que exiba a soma e a subtração de dois números.

---
+++ Q2Parte2.java
@@ -0,0 +1,13 @@
+public class Q2Parte2 {
+    public static void main(String[] args) {
+        
+        //Declarar variaveis
+        int n1 = 5;
+        int n2 = 4;
+        double soma = n1 + n2;
+        double sub = n1 - n2;
+
+        System.out.println("A soma é:" + soma);
+        System.out.println("A subtração é:" + sub);
+    }
+}
