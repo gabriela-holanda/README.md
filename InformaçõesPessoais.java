1f7cbeab4a4d0c9acab1a4d3ffdcd108e6202f61
gabriela-holanda <gabrielagithub@gmail.com>
2025-10-24 17:53:41 -0300

    Add files via upload
    Mostrar nome, idade e altura na mesma linha.

---
+++ InformaçõesPessoais.java
@@ -0,0 +1,12 @@
+public class InformaçõesPessoais {
+    public static void main(String[] args) {
+        //Declarar variaveis e tipos de dados
+
+        String nome = "Gabriela";
+        int idade = 15;
+        double altura = 1.60;
+
+        //Exibir dados
+        System.out.println("Meu nome é " + nome + ", tenho " + idade + " anos" + " e minha altura é " + altura + ".");
+    }
+}
